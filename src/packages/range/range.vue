cc42da5f499d2705ee1ab38441c7af4bc361a44b
chenqichun <1069012191@qq.com>
2022-04-26 18:01:11 +0800

    2022042618

--- src/packages/range/range.vue
+++ src/packages/range/range.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="cqc-range">
+    <div class="cqc-range-thumb"></div>
 
   </div>
 </template>
 
 <script>
+import { onMounted } from 'vue'
 export default {
   name: 'CqcRange',
+  props: {
+    thumbWidth: {
+      type: Number,
+      default: 30
+    },
+    height: {
+      type: Number,
+      default: 1
+    }
+  },
   setup() {
-    
+    onMounted
   }
 }
 </script>
